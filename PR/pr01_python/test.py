74b4a5a768ef009f242a230a70b513f512545e51
jh-ttu <juri.helekivi@taltech.ee>
2023-03-17 11:37:07 +0200

    Create test.py

---
+++ PR/pr01_python/test.py
@@ -0,0 +1,23 @@
+"""This is my first program.""" 
+name = "Ago"
+name = """Ago"""
+name = 'Ago'
+name = '''Ago'''
+print("Hello, ")
+print("B!")
+print()
+
+#TEST GRUPPP new3, multiple custom grades v334
+# persistent 4
+
+
+name = input('What is your name?')
+year = input(f'Hello, {name}! What year were you born in?')
+value = int(year)
+
+if value < 2008:
+    result = 2008 - value
+    print(f'You were {result} years old when Python 3.0 was released.')
+else:
+    result = value - 2008
+    print(f'Python 3 was {result} years old when you were born. ') 
